bb440fc8130aba0c24ef165218331075037060c4
Berkan Kaygusuz <berkankaygusuz41@outlook.com>
2023-04-27 17:36:03 +0300

    phonebook
    phonebooka geçildi,İşlemler düzeltildi
    
    Co-Authored-By: Baris Sener <39437925+barisener@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,39 +1,53 @@
-var notes = JSON.parse(localStorage.getItem("notes")) || [];
+var phonebook = [];
+
+function Person(name, number) {
+  this.name = name;
+  this.number = number;
+}
 
 while (true) {
-  var command = prompt("Komut girin (add/edit/delete/list/quit):");
+  var command = prompt("Komut girin (add/edit/delete/list/quit");
 
   if (command === "add") {
-    var note = prompt("Notunuzu girin:");
-    notes.push(note);
-    alert("Not eklendi!");
+    var name = prompt("isim giriniz");
+    var number = prompt("numara giriniz");
+    var person = new Person(name, number);
+    phonebook.push(person);
+    alert("Kişi eklendi!");
   } else if (command === "edit") {
-    console.log("Notlar:");
-    for (var i = 0; i < notes.length; i++) {
-      console.log(i + "- " + notes[i]);
-    }
-    var index = prompt("Düzenlenecek notun sırasını seçiniz:");
-    var newNote = prompt("Yeni notunuzu girin:");
-    notes[index] = newNote;
-    console.log("Not düzenlendi!");
+    console.log("kişi listesi:");
+    phonebook.forEach(function (person) {
+      console.log("İsmi: " + person.name + "  Numara:" + person.number);
+    });
+    var edited = prompt("Düzenlenecek kişinin ismini giriniz:");
+    if (phonebook.indexOf(edited) > -1) {
+      var name = prompt("isim giriniz");
+      var number = prompt("numara giriniz");
+      var person = new Person(name, number);
+      phonebook.splice(phonebook.indexOf(edited), 1, person);
+      console.log("Kişi düzenlendi!");
+    } else console.log("Kişi yok");
   } else if (command === "delete") {
     console.log("Notlar:");
-    for (var i = 0; i < notes.length; i++) {
-      console.log(i + "- " + notes[i]);
-    }
-    var index = prompt("Silinecek notun indeksini girin:");
-    notes.splice(index, 1);
-    console.log("Not silindi!");
+    console.log("kişi listesi:");
+    phonebook.forEach(function (person) {
+      console.log("İsmi: " + person.name + "  Numara:" + person.number);
+    });
+    var deleted = prompt("Silinecek kişinin ismini giriniz:");
+    if (phonebook.indexOf(deleted) > -1) {
+      phonebook.splice(phonebook.indexOf(deleted), 1);
+      console.log("Kişi silindi!");
+    } else console.log("Kişi bulunamadı!");
   } else if (command === "list") {
-    console.log("Notlar:");
-    for (var i = 0; i < notes.length; i++) {
-      console.log(i + "- " + notes[i]);
-    }
+    console.log("kişi listesi:");
+    phonebook.forEach(function (person) {
+      console.log("İsmi: " + person.name + "  Numara:" + person.number);
+    });
   } else if (command === "quit") {
     console.log("Uygulama kapatıldı.");
-    localStorage.setItem("notes", JSON.stringify(notes));
     break;
   } else {
     console.log("Geçersiz komut, lütfen tekrar deneyin.");
+    break;
   }
 }
