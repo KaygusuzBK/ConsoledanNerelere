ce1f9c7881d05c75e7c49f8cfb91456a1c5533a2
Berkan Kaygusuz <berkankaygusuz41@outlook.com>
2023-05-23 21:35:15 +0300

    EDİT ve Temizlik
    yenilikten ziyade biraz daha sadeleştirip toparlamaya çalıştım. ufak bi kaç fonksiyonları değiştirip üzerlerinde oynadım EDİT KISMINIDA HALLETMEYE ÇALIŞTIM :rocket:
    
    Co-Authored-By: Baris Sener <39437925+barisener@users.noreply.github.com>

--- app.js
+++ app.js
@@ -3,37 +3,75 @@ const number = document.querySelector("#number");
 const addBtn = document.querySelector("#addBtn");
 const searchBtn = document.querySelector("#searchBtn");
 const deleteBtn = document.querySelector("#deleteBtn");
-const updateBtn = document.querySelector("#updateBtn");
+const editBtn = document.querySelector("#editBtn");
+const tbody = document.querySelector("#tbody");
 
 function renderTable() {
   const phonebook = getPhonebook();
-  const tbody = document.querySelector("tbody");
+  const tbody = document.querySelector("#tbody");
   tbody.innerHTML = "";
-  phonebook.map(function (person) {
+  phonebook.forEach((person) => {
     let tr = document.createElement("tr");
-    let tname = document.createElement("td");
-    let tnumber = document.createElement("td");
-    let tdelete = document.createElement("td");
-
-    tname.innerHTML = person.name;
-    tnumber.innerHTML = person.number;
-    tdelete.innerHTML = `<button onclick="deleteButton(${person.id})">Delete</button>`;
-    tr.appendChild(tname);
-    tr.appendChild(tnumber);
-    tr.appendChild(tdelete);
+    tr.innerHTML = `
+    <td>${person.name}</td>
+    <td>${person.number}</td>
+    <td>
+      <button onclick="editButton(${person.id})">Edit</button>
+      <button onclick="deleteButton(${person.id})">Delete</button>
+    </td>
+  `;
     tbody.appendChild(tr);
-
   });
 }
 
-function getCurrentFormData() {
+function formSubmit(e) {
+  e.preventDefault();
   const name = document.querySelector("#name").value;
   const number = document.querySelector("#number").value;
-  return { name, number };
+  addPerson(name, number);
+  renderTable();
+  document.getElementById("addForm").reset();
 }
 
 function deleteButton(id) {
   deletePerson(id);
+  renderTable();
+}
+
+function editButton(id) {
+  const person = phonebook.find((person) => person.id === id);
+  if (person) {
+    document.querySelector("#name").value = person.name;
+    document.querySelector("#number").value = person.number;
+    document.getElementById("addBtn").style.display = "none";
+    document.getElementById("editBtn").style.display = "inline-block";
+    document.getElementById("editBtn").setAttribute("data-id", id);
+    document.getElementById("editBtn").onclick = function () {
+      editPersonData(id);
+    };
+  }
+}
+
+function editPersonData(id) {
+  const name = document.querySelector("#name").value;
+  const number = document.querySelector("#number").value;
+  editPerson(id, name, number);
+  renderTable();
+  document.getElementById("addForm").reset();
+  document.getElementById("addBtn").style.display = "inline-block";
+  document.getElementById("editBtn").style.display = "none";
+  document.getElementById("editBtn").removeAttribute("data-id");
+}
+
+function getPerson(id) {
+  const person = phonebook.find((person) => person.id === id);
+  return person;
+}
+
+function editPersonData(id) {
+  const name = document.querySelector("#name").value;
+  const number = document.querySelector("#number").value;
+  editPerson(id, name, number);
   renderTable();
 }
 
@@ -43,3 +81,20 @@ addBtn.onclick = () => {
   renderTable();
 };
 
+addBtn.onclick = () => {
+  const { name, number } = getCurrentFormData();
+  addPerson(name, number);
+  renderTable();
+};
+
+editBtn.onclick = () => {
+  const { name, number } = getCurrentFormData();
+  editPersonData(name, number);
+  renderTable();
+};
+
+function getCurrentFormData() {
+  const name = document.querySelector("#name").value;
+  const number = document.querySelector("#number").value;
+  return { name, number };
+}
