03c7dde3fc87bcee60415a7298767b9758a0cbac
Berkan Kaygusuz <berkankaygusuz41@outlook.com>
2023-05-26 19:31:38 +0300

    Search
    search işlemi yapılabilir hale getirildi.
     @barisener

--- app.js
+++ app.js
@@ -5,29 +5,34 @@ const searchBtn = document.querySelector("#searchBtn");
 const deleteBtn = document.querySelector("#deleteBtn");
 const editBtn = document.querySelector("#editBtn");
 const tbody = document.querySelector("#tbody");
+const restart = document.querySelector("#restart");
+
+function CreateTable(person) {
+  console.log(person);
+  let tr = document.createElement("tr");
+  tr.innerHTML = `
+  <td>${person.name}</td>
+  <td>${person.number}</td>
+  <td>
+    <button onclick="editButton(${person.id})">Edit</button>
+    <button onclick="deleteButton(${person.id})">Delete</button>
+  </td> `;
+  tbody.appendChild(tr);
+}
 
 function renderTable() {
   const phonebook = getPhonebook();
   const tbody = document.querySelector("#tbody");
   tbody.innerHTML = "";
   phonebook.forEach((person) => {
-    let tr = document.createElement("tr");
-    tr.innerHTML = `
-    <td>${person.name}</td>
-    <td>${person.number}</td>
-    <td>
-      <button onclick="editButton(${person.id})">Edit</button>
-      <button onclick="deleteButton(${person.id})">Delete</button>
-    </td>
-  `;
-    tbody.appendChild(tr);
+    CreateTable(person);
   });
 }
 
 function formSubmit(e) {
   e.preventDefault();
-  const name = document.querySelector("#name").value;
-  const number = document.querySelector("#number").value;
+  const name = document.querySelector("#name").value.trim();
+  const number = document.querySelector("#number").value.trim();
   addPerson(name, number);
   renderTable();
   document.getElementById("addForm").reset();
@@ -39,47 +44,23 @@ function deleteButton(id) {
 }
 
 function editButton(id) {
-  const person = phonebook.find((person) => person.id === id);
-  if (person) {
-    document.querySelector("#name").value = person.name;
-    document.querySelector("#number").value = person.number;
-    document.getElementById("addBtn").style.display = "true";
-    document.getElementById("editBtn").style.display = "inline-block";
-    document.getElementById("editBtn").setAttribute("data-id", id);
-    document.getElementById("editBtn").onclick = function () {
-      editPersonData(id);
-    };
-  }
+  const { name, number } = getPerson(id);
+  document.getElementById("editBtn").style.display = "inline-block";
+  setCurrentFormData(id, name, number);
 }
 
 function editPersonData(id) {
-  const name = document.querySelector("#name").value;
-  const number = document.querySelector("#number").value;
-  editPerson(id, name, number);
-  renderTable();
-  document.getElementById("addForm").reset();
-  document.getElementById("addBtn").style.display = "inline-block";
-  document.getElementById("editBtn").style.display = "none";
-  document.getElementById("editBtn").removeAttribute("data-id");
-}
-
-function getPerson(id) {
-  const person = phonebook.find((person) => person.id === id);
-  return person;
-}
-
-function editPersonData(id) {
-  const name = document.querySelector("#name").value;
-  const number = document.querySelector("#number").value;
-  editPerson(id, name, number);
-  renderTable();
-}
-
-addBtn.onclick = () => {
   const { name, number } = getCurrentFormData();
-  addPerson(name, number);
+  editPerson(id, name, number);
   renderTable();
-};
+}
+
+function searchPerson(searchWord) {
+  const filtered = phonebook.filter((person) => {
+    return person.name === searchWord || person.number === searchWord;
+  });
+  return filtered;
+}
 
 addBtn.onclick = () => {
   const { name, number } = getCurrentFormData();
@@ -93,8 +74,37 @@ editBtn.onclick = () => {
   renderTable();
 };
 
+searchBtn.onclick = () => {
+  const tbody = document.querySelector("#tbody");
+  tbody.innerHTML = "";
+  const { searchWord } = getCurrentSearchData();
+  const persons = searchPerson(searchWord);
+  console.log(persons);
+  persons.forEach((person) => {
+    CreateTable(person);
+    console.log(person);
+  });
+};
+
+restart.onclick = () => {
+  renderTable();
+};
+
 function getCurrentFormData() {
-  const name = document.querySelector("#name").value;
-  const number = document.querySelector("#number").value;
+  const name = document.querySelector("#name").value.trim();
+  const number = document.querySelector("#number").value.trim();
   return { name, number };
+}
+
+function getCurrentSearchData() {
+  const searchWord = document.querySelector("#searchWord").value.trim();
+  return { searchWord };
+}
+
+function setCurrentFormData(id, name, number) {
+  document.querySelector("#name").value = name;
+  document.querySelector("#number").value = number;
+  document.getElementById("editBtn").onclick = function () {
+    editPersonData(id);
+  };
 }
